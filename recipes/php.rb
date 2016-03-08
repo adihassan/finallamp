9a00290b941e42615d614ccdb0fab87d127a2f3f
adnan.hassan <adihassan@live.com>
2016-03-08 22:17:03 +0500

    changes made

--- recipes/php.rb
+++ recipes/php.rb
@@ -1,13 +1,2 @@
 
 include_recipe 'php'
-
-
-
-#hc = php_pear_channel "pear.pollinimini.net" do
-#  action :discover
-#end
-#php_pear "Protobuf" do
-#  preferred_state "beta"
-#  channel hc.channel_name
-#  action :install
-#end
