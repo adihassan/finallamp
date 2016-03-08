19a640f3af684500072f8ac32396820b19443342
adnan.hassan <adihassan@live.com>
2016-03-08 21:27:32 +0500

    mysql changes

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -4,14 +4,3 @@ mysql_service 'd' do
   initial_root_password '121212'
   action [:create, :start]
 end
-mysql_connection_info = {
-  :host     => 'localhost',
-  :username => 'root',
-  :password => node[:mysql][:server_root_password]
-}
-
-mysql_database_user 'test' do
-  connection mysql_connection_info
-  password   '121212'
-  action     :create
-end
